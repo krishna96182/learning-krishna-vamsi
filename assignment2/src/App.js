982487fff1e457cc169ee3634fa5715875b7d637
Krishna Gangisetty <krishna.gangisetty@ascendion.com>
2024-07-24 17:20:50 +0530

    Assignment2

---
+++ assignment2/src/App.js
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import './App.css';
+import Menu from './Menu';
+import Login from './Login';
+import Footer from './Footer';
+import Container from './Container'
+import Counter from './Counter';
+import Todo from './Todo';
+import Addtwo from './Addtwo'
+import Hobby from './Hobby';
+import Calculator from './Calculator';
+import Student from './Student';
+import Gender from './Genders';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Display from './Display';
+import Gitdata from './Gitdata';
+function App() {
+  return (
+    <div className="App">
+      <header>
+        {/* <Hobby/>
+        <Addtwo/>
+        <Calculator/>
+        <Student/> */}
+        {/* <h1>Main Page</h1>
+        <BrowserRouter>
+        <Link to="/todo">Todo</Link>
+        <Link to="/login/Lets login/123">Login</Link>
+        <br/>
+        <Routes>
+          <Route path="/todo" element={<Todo/>}/>
+          <Route path="/login/:title/:tokenId" element={<Login/>}/>
+        </Routes>
+        </BrowserRouter> */}
+        {/* <Gender/>
+        <Display/> */}
+        <Gitdata/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
